1e96227f576c0e7b5c4e39d24ba51c773a46e6d7
John M Naglick <jnaglick@gmail.com>
2019-12-06 01:37:46 -0500

    some quick features (different rooms (randomly assigned), user list, typing in progress indicator) showing need for better state mgmt

--- src/client.tsx
+++ src/client.tsx
@@ -1,13 +1,132 @@
-//import { h, render, Component } from 'preact';
-const io = require('socket.io-client');
+import { h, render, Component } from 'preact';
+const socketClient = require('socket.io-client');
 
-var socket = io();
 
-//const socket = io_client();
-const random = Math.random();
-console.log(`sending ${random}`)
-socket.emit('chat message', `client sez ${Math.random()}`);
+const ChatUserList = ({ users }) => {
+    return <ul>
+        {
+            users.map((user) => 
+                <li>
+                    { user.id }
+                </li>
+            )
+        }
+    </ul>
+}
 
-socket.on('chat message reply', function(msg){
-    console.log(`received from server: ${msg}`);
-});
+class Chat extends Component<any, any> {
+    chatSocket: any;
+
+    messageSubmit: any;
+    messageKeydown: any;
+
+    constructor() {
+        super();
+
+        const roomNumber = Math.floor(Math.random()*5)
+
+        this.state = {
+            roomNumber,
+            userList: [],
+            msgs: [],
+            someoneIsTyping: false,
+            inProgressMsg: '', // form
+        }
+
+        this.chatSocket = socketClient(`/chat/${roomNumber}`, { transports: ['websocket'] });
+
+        const fetchUserList = () => {
+            this.chatSocket.emit('user_list', ({ users: userList }) => {
+                console.log('received userList')
+                this.setState({
+                    userList
+                })
+            });
+        }
+        fetchUserList();
+
+        this.chatSocket.on('user_joined_chat', ({ user }) => {
+            this.setState({
+                msgs: [ ...this.state.msgs, `Someone joined: ${user.id}` ]
+            });
+            fetchUserList();
+        });
+
+        this.chatSocket.on('user_left_chat', ({ user }) => {
+            this.setState({
+                msgs: [ ...this.state.msgs, `Someone left: ${user.id}` ]
+            });
+            fetchUserList();
+        });
+
+        this.chatSocket.on('message_from_user', ({ user, message }) => {
+            this.setState({
+                msgs: [ ...this.state.msgs, `${user.id}: ${message}` ]
+            })
+        });
+
+        this.chatSocket.on('typing_from_user', ({ user }) => {
+            if (this.state.someoneIsTyping) {
+                clearTimeout(this.state.someoneIsTyping.timer)
+            }
+
+            this.setState({
+                someoneIsTyping: {
+                    user,
+                    timer: setTimeout(() => {
+                        this.setState({
+                            someoneIsTyping: false
+                        })
+                    }, 500)
+                }
+            })
+        })
+
+        this.messageSubmit = (event) => {
+            this.chatSocket.emit('message_from_user', this.state.inProgressMsg);
+
+            this.setState({
+                inProgressMsg: ''
+            })
+
+            event.preventDefault();
+        }
+
+        this.messageKeydown = (event) => {
+            this.chatSocket.emit('typing_from_user', true);
+
+            this.setState({
+                inProgressMsg: event.target.value
+            })
+        }
+    }
+
+    render({}, { roomNumber, userList, msgs, someoneIsTyping, inProgressMsg }) {
+        return <div>
+            Room#{ roomNumber }...
+            <ul>
+                {
+                    msgs.map((msg) => 
+                        <li>
+                            { msg }
+                        </li>
+                    )
+                }
+            </ul>
+
+            <ChatUserList users={ userList }/>
+
+            <form onSubmit={ this.messageSubmit }>
+                <input type="text" value={ inProgressMsg } onKeyDown={ this.messageKeydown }></input>
+                <input type="submit" style="display: none" />
+            </form>
+
+            <div>
+                { !!someoneIsTyping && <span>{ someoneIsTyping.user.id } is typing...</span> }
+            </div>
+        </div>
+    }
+}
+
+
+render(<Chat />, document.body);
--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,70 @@
+import { runInThisContext } from "vm";
+
 const express = require('express')
 const http = require('http')
 const socketio = require('socket.io')
-require('socket.io-client');
 
 const app = express();
-const server = http.createServer(app);
-const io_server = socketio(server, { serveClient: false });
+const httpServer = http.createServer(app);
+const socketServer = socketio(httpServer, { serveClient: false, transports: ['websocket'] });
 
 app.use(express.static('dist/static'));
 
-io_server.on('connection', function(socket){
-    console.log('a user connected');
+class User {
+  id: string;
+  constructor(id: string) {
+    this.id = id;
+  }
+}
 
-    socket.on('chat message', function(msg){
-        console.log('received from client: ' + msg);
-        io_server.emit('chat message reply', `server got a msg: "${msg}"`);
-    });
-});
+class Chat {
+  name: string;
+  users: Array<User>;
 
-server.listen(3000, function(){
-  console.log('listening on *:3k');
+  constructor(name) {
+    this.name = name
+    this.users = []
+  }
+}
+
+const ChatDB: Array<Chat> = []
+
+socketServer.of(/^\/chat\/\d+$/).on('connection', function(socket) {
+  console.log(`got connection to ${socket.nsp.name}`);
+
+  const chatId = socket.nsp.name.match(/\d+/)[0];
+  let chat = ChatDB.find((chat) => chat.name == chatId);
+  if (!chat) {
+    chat = new Chat(chatId);
+    ChatDB.push(chat);
+  }
+
+  const user = new User(socket.id);
+
+  chat.users = [ ...chat.users, user ];
+
+  socket.broadcast.emit('user_joined_chat', { user });
+
+  socket.on('disconnect', (reason) => {
+    chat.users = chat.users.filter((u) => u.id != user.id);
+    // TODO check if should destroy Chat
+    socket.broadcast.emit('user_left_chat', { user });
+  });
+
+  socket.on('user_list', (callback) => {
+    callback({ users: chat.users })
+  });
+
+  socket.on('message_from_user', (message) => {
+    socket.broadcast.emit('message_from_user', { user, message })
+  });
+
+  socket.on('typing_from_user', () => {
+    socket.broadcast.emit('typing_from_user', { user })
+  });
+})
+
+
+httpServer.listen(4000, function() {
+  console.log('listening on *:4k');
 });
